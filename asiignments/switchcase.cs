298c1d60ef745a7183624e0e43ead58e7c8c0625
SnehalKawade <snehkawade@gmail.com>
2023-06-13 12:55:49 +0530

    switch cases

---
+++ asiignments/switchcase.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//A bank intends to design a program to display the denomination of an input amount. 
+//The available denomination with the bank are of rupees 2000, 500, 200, 100, 50, 20, 10 and 1.
+
+namespace asiignments
+{
+    internal class switchcase
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Console.WriteLine("Enter two number =");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1.Addition");
+            Console.WriteLine("2.Substraction");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Division");
+            Console.WriteLine("5.Modulus");
+            Console.WriteLine("Enter a number to select option =");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1: 
+                    int sum = n1+ n2;
+                    Console.WriteLine("The addition is "+sum);
+                    break;
+
+                case 2:
+                    int sub = n1 - n2;
+                    Console.WriteLine("The substraction is "+sub);
+                    break;
+
+                case 3: 
+                    int mul = n1* n2;
+                    Console.WriteLine("The multiplication is "+mul);
+                    break;
+
+                case 4:
+                    double div =(double) n1/ n2;
+                    Console.WriteLine("The division is "+div);
+                    break;
+
+                case 5:
+                    int mod = n1 % n2;
+                    Console.WriteLine("The modulus is "+mod);
+                    break;
+
+                default:
+                    Console.WriteLine("You have entered wrong option...!");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ asiignments/switchcase2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//wap to print 1-7 week days using switch case
+
+namespace asiignments
+{
+    internal class switchcase2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("1. Sunday");
+                    break;
+
+                case 2:
+                    Console.WriteLine("2. monday");
+                    break;
+
+                case 3:
+                    Console.WriteLine("3. Tuesday");
+                    break;
+
+                case 4:
+                    Console.WriteLine("4. Wedesday");
+                    break;
+
+                case 5:
+                    Console.WriteLine("5. Thursday");
+                    break;
+
+                case 6: 
+                    Console.WriteLine("6. Friday");
+                    break;
+
+                case 7:
+                    Console.WriteLine("7. Saturday");
+                    break;
+
+                default:
+                    Console.WriteLine("You entered wrong number...!");
+                    break;
+            }
+        }
+    }
+}
